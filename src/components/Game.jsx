adf2fce7d1d8b0eb79ae104c7be31e703775c004
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-12-10 16:15:27 +0530

    added platform icons

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import PlatformIconList from "./PlatformIconList";
 
-const Game = ({game}) => {
+const Game = ({ game }) => {
+   console.log(game);
   return (
-    <div className='game-card'>
+    <div className="game-card">
       <img src={game.background_image} alt="game" />
       <h1>{game.name}</h1>
+      {/* <div>
+        {game.parent_platforms.map((platform, index) => (
+          <h3 key={index}>{platform.platform.name}</h3>
+        ))}
+      </div> */}
+      <PlatformIconList key={game.slug} platforms={game.parent_platforms} />
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
