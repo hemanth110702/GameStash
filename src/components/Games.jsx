00dce2efad6a2fe93d6a850c99b9af7f3b2541d5
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-12-10 13:35:14 +0530

    fetched the games

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,7 +1,25 @@
-const Games = () => {
-  return (
-    <div className="games">Games</div>
-  )
-}
+import { useEffect } from "react";
+import { useState } from "react";
+import apiClient from "../services/apiClient";
 
-export default Games
+const Games = () => {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  },[]);
+
+  return (
+    <>
+      <div className="games">Games</div>
+      {error && <h1>{error}</h1>}
+      {games.map(game => <li key={game.id}>{game.name}</li>)}
+    </>
+  );
+};
+
+export default Games;
