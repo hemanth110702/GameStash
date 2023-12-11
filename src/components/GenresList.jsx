4496dd7ced7bee729f3d1bab456e770d41edcf7d
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-12-11 21:22:57 +0530

    added static genre data

--- src/components/GenresList.jsx
+++ src/components/GenresList.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 import useGenres from "../hooks/useGenres";
 import getOptimizedImage from "../services/image-url";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 
 const GenresList = ({ selectedGenre, setSelectedGenre }) => {
-  const [loading, setLoading] = useState(false);
-  const { genres } = useGenres(setLoading);
+  const genres = useGenres();
   return (
     <ul>
-      {loading && (
-        <SkeletonTheme baseColor="#202020" highlightColor="#444">
-          {[...Array(20)].map((_, index) => (
-            <div key={index}>
-              <Skeleton variant="h2" width={300} />
-              <div style={{ height: 20 }} />
-            </div>
-          ))}
-        </SkeletonTheme>
-      )}
       {genres.map((genre) => (
         <li key={genre.id}>
           <img src={getOptimizedImage(genre.image_background)} alt="" />
--- src/hooks/useGenres.jsx
+++ src/hooks/useGenres.jsx
@@ -1,32 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
-import { CanceledError } from "axios";
+import genres from "../data/genres"
 
-const useGenres = (setLoading) => {
-  const [genres, setGenres] = useState([]);
-  const [error, setError] = useState("");
-
-  
-  useEffect(() => {
-    setLoading(true);
-    const controller = new AbortController();
-
-    apiClient
-      .get("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error };
+const useGenres = () => {
+  console.log("useGenres", genres);
+  return genres;
 };
 
 export default useGenres;
