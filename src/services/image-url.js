d4776e141ae3f541cd549bb885c1bffbd5885387
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-12-11 18:31:52 +0530

    handles games with no image

--- src/services/image-url.js
+++ src/services/image-url.js
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image.jpg"
+
 const getOptimizedImage = (url) => {
-  if(!url)return;
+  if(!url)return noImage;
   const target = "/media";
   const index = url.indexOf(target, 14) + target.length;
   const optimizedImageUrl = url.slice(0, index) + "/crop/600/400" + url.slice(index);
