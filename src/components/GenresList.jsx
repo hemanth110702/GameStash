060e86ecedd9f8429d8a54921c6a97cff60e5e09
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-12-19 08:52:21 +0530

    added hover effects for genres

--- src/components/GenresList.jsx
+++ src/components/GenresList.jsx
@@ -1,28 +1,32 @@
 import useGenres from "../hooks/useGenres";
 import getOptimizedImage from "../services/image-url";
 
-const GenresList = ({ selectedGenre, setSelectedGenre }) => {
+const GenresList = ({ selectedGenre, setSelectedGenre, darkTheme }) => {
   const genres = useGenres();
   return (
     <ul>
       {genres.map((genre) => (
         <li
+          onClick={() => setSelectedGenre(genre)}
           key={genre.id}
           style={{
             borderRadius: selectedGenre?.id === genre.id ? "8px" : "",
             backgroundColor:
-              selectedGenre?.id === genre.id ? "rgba(255, 255, 255, 0.21)" : "",
+              selectedGenre?.id === genre.id
+                ? darkTheme
+                  ? "rgba(255, 255, 255, 0.21)"
+                  : "rgba(0, 0, 0, 0.21)"
+                : "",
           }}
         >
           <img src={getOptimizedImage(genre.image_background)} alt="" />
-          <button
+          <p
             style={{
               fontWeight: selectedGenre?.id === genre.id ? "700" : "",
             }}
-            onClick={() => setSelectedGenre(genre)}
           >
             {genre.name}
-          </button>
+          </p>
         </li>
       ))}
     </ul>
