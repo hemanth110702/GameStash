f2310550a9ca42b508e18047346fb5096289426c
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2025-03-30 22:17:23 +0530

    [Dev]: added speed Insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
